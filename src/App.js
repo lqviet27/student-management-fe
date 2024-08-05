e85bcb37f7b1c05ef52879def2368feb4b6f68fa
Le Quoc Viet <lequocviet2752004@gmail.com>
2024-08-05 08:46:53 +0700

    complete header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="app-container">
-       <button className="btn btn-primary">Click me</button>
+      <Header/> 
     </div>
   );
 }
