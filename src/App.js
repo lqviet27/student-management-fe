2a149232dd3b680aced60a9efa35afff2980887f
Le Quoc Viet <lequocviet2752004@gmail.com>
2024-08-05 12:45:54 +0700

    test route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app-container">
       <Header/> 
+      <div>
+        test link
+        <div>
+          <button>
+            <Link to="/users">go to user page</Link>
+          </button>
+          <button>
+            <Link to="/admin">go to admin page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import User from './components/User/User';
+import Admin from './components/Admin/Admin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/users" element={<User />} />
+        <Route path="/admins" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
